ce30a89a8fa558aebb8fdc841e8fd8f9f911537b
Ulysse Rubens <urubens@uliege.be>
2022-05-09 15:19:11 +0200

    fix: improve CompanionFile

--- grails-app/controllers/be/cytomine/api/image/RestImageInstanceController.groovy
+++ grails-app/controllers/be/cytomine/api/image/RestImageInstanceController.groovy
@@ -22,8 +22,6 @@ import be.cytomine.Exception.InvalidRequestException
 import be.cytomine.api.RestController
 import be.cytomine.image.ImageInstance
 import be.cytomine.image.SliceInstance
-import be.cytomine.image.multidim.ImageGroup
-import be.cytomine.meta.Property
 import be.cytomine.ontology.UserAnnotation
 import be.cytomine.project.Project
 import be.cytomine.security.SecUser
