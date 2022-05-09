7252512a921b3f19b0905ff186fb2a923e6fa5a4
Ulysse Rubens <urubens@uliege.be>
2022-05-09 15:02:15 +0200

    fix: fix variable names in job execute by processing server url mapping

--- grails-app/conf/JobUrlMappings.groovy
+++ grails-app/conf/JobUrlMappings.groovy
@@ -33,7 +33,7 @@ class JobUrlMappings {
         "/api/job/$id/copy.$format" (controller : "restJob") {
             action = [POST : "copy"]
         }
-        "/api/job/$job_id/processing_server/$processing_server_id/execute.$format" (controller: "restJob") {
+        "/api/job/$jobId/processing_server/$processingServerId/execute.$format" (controller: "restJob") {
             action = [POST: "executeWithProcessingServer", GET: "executeWithProcessingServer"]
         }
         "/api/job/$id/kill.$format"(controller: "restJob") {
